b866b1de663ca104beee40cf4d72cfdc03682df0
shauryaps_08 <shauryapratapsingh060208@gmail.com>
2026-07-14 07:00:51 +0000

    after linting

--- src/main/scala/HardfloatSmokeTest.scala
+++ src/main/scala/HardfloatSmokeTest.scala
@@ -7,17 +7,17 @@ import hardfloat.AddRecFN
 // so this compiles only if the hardfloat dependency is wired correctly.
 class HardfloatSmokeTest(expWidth: Int = 8, sigWidth: Int = 24) extends Module {
   val io = IO(new Bundle {
-    val a  = Input(UInt((expWidth + sigWidth + 1).W))
-    val b  = Input(UInt((expWidth + sigWidth + 1).W))
+    val a            = Input(UInt((expWidth + sigWidth + 1).W))
+    val b            = Input(UInt((expWidth + sigWidth + 1).W))
     val roundingMode = Input(UInt(3.W))
-    val out = Output(UInt((expWidth + sigWidth + 1).W))
+    val out          = Output(UInt((expWidth + sigWidth + 1).W))
   })
 
   val adder = Module(new AddRecFN(expWidth, sigWidth))
-  adder.io.subOp := false.B
-  adder.io.a := io.a
-  adder.io.b := io.b
-  adder.io.roundingMode := io.roundingMode
+  adder.io.subOp          := false.B
+  adder.io.a              := io.a
+  adder.io.b              := io.b
+  adder.io.roundingMode   := io.roundingMode
   adder.io.detectTininess := false.B
 
   io.out := adder.io.out
